a2452e9019091aede4d72cd3d4348e3321343d80
Nikhil Utane <nikhil.subscribed@gmail.com>
2020-03-30 12:30:44 +0530

    minor UI enhancements

--- front-end/twitter.js
+++ front-end/twitter.js
@@ -64,15 +64,15 @@ button.addEventListener('click', () => {
     button.style.backgroundColor = "#ffc477"
     button.textContent = "Running";    
     
+    document.getElementById("in-progress").textContent = "Hold on, babies are sleeping. Waking them up. This can take upto a minute so be patient..."
     clearDataFromUI()
     showSpinner()
     var models = ["left", "right"];
-    models.forEach(function(model) {
-   
+
+    models.forEach(function(model) {   
       fetchRetry(`${API_ENDPOINT}?prompt=${prompt}&model=${model}`, 10000,6, {
         method: 'GET',
         headers: { 'content-type': 'application/json' },
-
       }).then((response) =>{
           return response.json()
       }).then((data) => {
@@ -86,7 +86,7 @@ button.addEventListener('click', () => {
             removeSpinner()
           }  
       }).catch(function(error){
-       
+        console.log("Got error for " + model)
       });;
     });
 });
@@ -119,6 +119,8 @@ function clearDataFromUI(data, side){
 
 function addDataToUI(data, side){    
     let area = null;
+    document.getElementById("in-progress").textContent = ""
+
     if(side === 'left'){ 
       area = tweetArea.childNodes[1];
     }else{ 
@@ -228,4 +230,35 @@ function constructTweetHTML(tweet, side){
       </div>
     </article>
   </div> `
+}
+
+// Get the modal
+var modal = document.getElementById("aboutModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("aboutBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+var btn = document.getElementById("gitBtn");
+btn.onclick = function() {
+  window.open("https://github.com/nikhilno1/bot-baby");
 }
