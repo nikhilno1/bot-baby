cf56f0a159b6317ecc8731f0f7beddba03709fde
Nikhil <nikhil.subscribed@gmail.com>
2020-04-13 11:52:31 +0530

    Added comments

--- deployment/app/update_file_s3.sh
+++ deployment/app/update_file_s3.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# Download the latest prompts file from S3, update it with new additions and push it back
+
 aws s3 cp s3://botbaby.in/files/prompts.txt /home/ubuntu/prompts_temp.txt
 sort /home/ubuntu/new_prompts.txt |uniq > /home/ubuntu/unique_prompts.txt
 cat /home/ubuntu/unique_prompts.txt >> /home/ubuntu/prompts_temp.txt
